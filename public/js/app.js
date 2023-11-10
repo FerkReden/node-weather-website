57551a0c4c5cd6c104f5b094d712e9b517be5a9b
FerkReden <depper731@gmail.com>
2023-11-10 18:01:22 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const location = search.value;
+    const location = search.value
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
@@ -14,12 +14,11 @@ weatherForm.addEventListener('submit', (e) => {
     fetch('/weather?address=' + location).then((response) => {    
         response.json().then((data) => {
             if (data.error) {
-                messageOne.textContent = data.error;
+                messageOne.textContent = data.error
             } else {
-                messageOne.textContent = data.location;
-                messageTwo.textContent = data.forecast;
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
-
 })
--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geocode = require('./utils/geocode.js')
-const forecast = require('./utils/forecast.js')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-// Defien path for Express config
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-// Setup hendelbars engine and views location
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-// Setup static directory to  serve
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -54,13 +54,13 @@ app.get('/weather', (req, res) => {
             return res.send({ error })
         }
 
-        forecast(latitude, longitude, (error, forcastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return res.send({ error })
             }
 
             res.send({
-                forecast: forcastData,
+                forecast: forecastData,
                 location,
                 address: req.query.address,
             })
